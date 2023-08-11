86fefe0c2b2af56f3ca3146c2108bb2c95499697
olayasturias <olaya_93@hotmail.es>
2023-08-11 17:14:02 +0200

    compiling successfuly. Launch files added

--- include/imc_ros_bridge/imc_ros_bridge_server.hpp
+++ include/imc_ros_bridge/imc_ros_bridge_server.hpp
@@ -10,11 +10,8 @@
  * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 #ifndef IMC_ROS_BRIDGE_SERVER_H
 #define IMC_ROS_BRIDGE_SERVER_H
-
-#include <memory>
 
 #include "rclcpp/rclcpp.hpp"
 #include <imc_tcp_link/imc_handle.h>
@@ -25,12 +22,11 @@ namespace ros_to_imc {
 template <typename ROS_MSG, typename IMC_MSG>
 bool convert(const ROS_MSG& ros_msg, IMC_MSG& imc_msg)
 {
-    static_assert(sizeof(IMC_MSG) == -1 || sizeof(ROS_MSG) == -1, "ERROR: You need to supply a convert specialization for the ROS -> IMC msg types provided");
+    // static_assert(sizeof(IMC_MSG) == -1 && sizeof(ROS_MSG) == -1, "ERROR: You need to supply a convert specialization for the ROS -> IMC msg types provided");
     return false;
 }
 
 template <typename ROS_MSG, typename IMC_MSG>
-
 class BridgeServer : public rclcpp::Node
 {
 
@@ -43,22 +39,25 @@ class BridgeServer : public rclcpp::Node
 
 public:
 
-    BridgeServer(IMCHandle& imc_handle, const std::string& ros_topic) : imc_handle(imc_handle)
+    BridgeServer(IMCHandle& imc_handle, const std::string& ros_topic) : rclcpp::Node("bridge_server"), imc_handle(imc_handle)
     {
-        // ros_sub = ros_node.subscribe(ros_topic, 10, &BridgeServer::conversion_callback, this);
-        ros_sub = this->create_subscription<ROS_MSG>(ros_topic, &BridgeServer::conversion_callback, this,_1);
+        // ros_sub = this->create_subscription<ROS_MSG>(ros_topic, &BridgeServer::conversion_callback, this,_1);
+        ros_sub = this->create_subscription<ROS_MSG>(
+            ros_topic, rclcpp::QoS(10), [this](const typename ROS_MSG::SharedPtr msg) {
+                this->conversion_callback(*msg);
+            });
     }
 
     void conversion_callback(const ROS_MSG& ros_msg)
     {
         IMC_MSG imc_msg;
-        bool success = convert(ros_msg, imc_msg);
+        bool success = convert(imc_msg, static_cast<const ROS_MSG&>(ros_msg));
         if (success) {
             //tcp_client_.write(&imc_msg);
             imc_handle.write(imc_msg);
         }
         else {
-            ROS_WARN("There was an error trying to convert imc type %s", imc_msg.getName());
+            RCLCPP_WARN(rclcpp::get_logger("bridge_server"),"There was an error trying to convert imc type %s", imc_msg.getName());
         }
     }
 
@@ -71,7 +70,7 @@ namespace imc_to_ros {
 template <typename IMC_MSG, typename ROS_MSG>
 bool convert(const IMC_MSG& imc_msg, ROS_MSG& ros_msg)
 {
-    static_assert(sizeof(IMC_MSG) == -1 || sizeof(ROS_MSG) == -1, "ERROR: You need to supply a convert specialization for the IMC -> ROS msg types provided");
+    // static_assert(sizeof(IMC_MSG) == -1 && sizeof(ROS_MSG) == -1, "ERROR: You need to supply a convert specialization for the IMC -> ROS msg types provided");
     return false;
 }
 
@@ -87,7 +86,7 @@ private:
 
 public:
 
-    BridgeServer(IMCHandle& imc_handle, rclcpp::Node& ros_node, const std::string& ros_topic)
+    BridgeServer(IMCHandle& imc_handle, rclcpp::Node& ros_node, const std::string& ros_topic) : rclcpp::Node("bridge_server")
     {
         ros_pub = this->create_publisher<ROS_MSG>(ros_topic, 10);
         imc_handle.tcp_subscribe(IMC_MSG::getIdStatic(), std::bind(&BridgeServer::conversion_callback, this, std::placeholders::_1));
@@ -98,7 +97,9 @@ public:
         ROS_MSG ros_msg;
         bool success = convert(static_cast<const IMC_MSG&>(*imc_msg), ros_msg);
         if (success) {
-            ros_pub.publish(ros_msg);
+            // ros_pub.publish(ros_msg);
+            // ros2 publish
+            ros_pub->publish(ros_msg);
         }
         else {
             RCLCPP_WARN(rclcpp::get_logger("ros_img_logger"),"There was an error trying to convert imc type %s", imc_msg->getName());
--- src/bridge_node.cpp
+++ src/bridge_node.cpp
@@ -14,13 +14,14 @@
 #include "rclcpp/rclcpp.hpp"
 #include <iostream>
 
+
 #include <imc_ros_bridge/imc_ros_bridge_server.hpp>
 
 #include <imc_ros_bridge/ros_to_imc/Heartbeat.h>
 #include <imc_ros_bridge/ros_to_imc/GpsFix.h>
 #include <imc_ros_bridge/ros_to_imc/Goto.h>
 #include <imc_ros_bridge/ros_to_imc/GpsNavData.h>
-#include <imc_ros_bridge/ros_to_imc/EstimatedState.h>
+#include <imc_ros_bridge/ros_to_imc/EstimatedState.hpp>
 #include <imc_ros_bridge/ros_to_imc/PlanControlState.h>
 #include <imc_ros_bridge/ros_to_imc/VehicleState.hpp>
 #include <imc_ros_bridge/ros_to_imc/RemoteState.hpp>
@@ -39,13 +40,21 @@
 #include <imc_ros_bridge/imc_to_ros/PlanDB.hpp>
 #include <imc_ros_bridge/imc_to_ros/PlanControl.h>
 
-
 using namespace std;
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "imc_bridge");
-    rclcpp::Node ros_node;
+    rclcpp::init(argc, argv);
+    // rclcpp::spin(std::make_shared<MinimalSubscriber>());
+    
+    // ros::Node ros_node;
+    // write ros node in ros2
+    rclcpp::Node ros_node("imc_ros_bridge");
+    // Create a ROS node with a specified name
+    // rclcpp::NodeOptions options;
+    // options.node_name("imc_ros_bridge");
+    // auto ros_node = std::make_shared<rclcpp::Node>("imc_ros_bridge", options);
+
 
     std::string neptus_addr;
     std::string bridge_tcp_addr;
@@ -53,51 +62,60 @@ int main(int argc, char** argv)
     std::string sys_name;
     int imc_id;
 	int imc_src;
-    ros::param::param<std::string>("~neptus_addr", neptus_addr, "127.0.0.1");
-    ros::param::param<std::string>("~bridge_addr", bridge_tcp_addr, "127.0.0.1");
-    ros::param::param<std::string>("~bridge_port", bridge_tcp_port, "6002");
-    ros::param::param<std::string>("~system_name", sys_name, "imc_ros_bridge");
+    rclcpp::Parameter neptus_addr_param("~neptus_addr", neptus_addr);
+    rclcpp::Parameter bridge_tcp_addr_param("~bridge_addr", bridge_tcp_addr);
+    rclcpp::Parameter bridge_tcp_port_param("~bridge_port", bridge_tcp_port);
+    rclcpp::Parameter sys_name_param("~system_name", sys_name);
+
 	// Neptus seems to have swapped these values for SOME REASON. so I swap here too...
 	// tldr: imc_id is the vehicle TYPE and imc_src is the SPECIFIC VEHICLE
-    ros::param::param<int>("~imc_id", imc_src, 30);
-	ros::param::param<int>("~imc_src", imc_id, 5);
+    rclcpp::Parameter imc_src_param("~imc_src", imc_src);
+    rclcpp::Parameter imc_id_param("~imc_id", imc_id);
 
     IMCHandle imc_handle(bridge_tcp_addr, bridge_tcp_port, neptus_addr, sys_name, imc_id, imc_src);
 
-    ros_to_imc::BridgeServer<std_msgs::msg::Empty, IMC::Heartbeat> heartbeat_server(ros_node, imc_handle, "heartbeat");
-    ros_to_imc::BridgeServer<sensor_msgs::NavSatFix, IMC::GpsFix> gpsfix_server(ros_node, imc_handle, "gps_fix");
-    ros_to_imc::BridgeServer<geometry_msgs::msg::Pose, IMC::Goto> goto_server_dummy(ros_node, imc_handle, "goto_input");
-    ros_to_imc::BridgeServer<sensor_msgs::NavSatFix, IMC::GpsNavData> gpsnavdata_server(ros_node, imc_handle, "gps_nav_data");
-    ros_to_imc::BridgeServer<imc_ros_bridge::EstimatedState, IMC::EstimatedState> estimatedstate_server(ros_node, imc_handle, "estimated_state");
-    ros_to_imc::BridgeServer<imc_ros_bridge::PlanControlState, IMC::PlanControlState> plancontrolstate_server(ros_node, imc_handle, "plan_control_state");
-    ros_to_imc::BridgeServer<imc_ros_bridge::VehicleState, IMC::VehicleState> vehiclestate_server(ros_node, imc_handle, "vehicle_state");
-    ros_to_imc::BridgeServer<imc_ros_bridge::RemoteState, IMC::RemoteState> RemoteState_server(ros_node, imc_handle, "remote_state");
-    ros_to_imc::BridgeServer<imc_ros_bridge::SonarData, IMC::SonarData> sonar_server(ros_node, imc_handle, "sonar_data");
-    ros_to_imc::BridgeServer<imc_ros_bridge::DesiredHeading, IMC::DesiredHeading> DesiredHeading_server(ros_node, imc_handle, "desired_heading");
-    ros_to_imc::BridgeServer<imc_ros_bridge::DesiredHeadingRate, IMC::DesiredHeadingRate> DesiredHeadingRate_server(ros_node, imc_handle, "desired_heading_rate");
-    ros_to_imc::BridgeServer<imc_ros_bridge::DesiredPitch, IMC::DesiredPitch> DesiredPitch_server(ros_node, imc_handle, "desired_pitch");
-    ros_to_imc::BridgeServer<imc_ros_bridge::DesiredRoll, IMC::DesiredRoll> DesiredRoll_server(ros_node, imc_handle, "desired_roll");
-    ros_to_imc::BridgeServer<imc_ros_bridge::DesiredSpeed, IMC::DesiredSpeed> DesiredSpeed_server(ros_node, imc_handle, "desired_speed");
-    ros_to_imc::BridgeServer<imc_ros_bridge::DesiredZ, IMC::DesiredZ> DesiredZ_server(ros_node, imc_handle, "desired_z");
-    ros_to_imc::BridgeServer<imc_ros_bridge::PlanDB, IMC::PlanDB> to_imc_plandb_server(ros_node, imc_handle, "plan_db");
+    ros_to_imc::BridgeServer<std_msgs::msg::Empty, IMC::Heartbeat> heartbeat_server(imc_handle, "heartbeat");
+    // ros_to_imc::BridgeServer<sensor_msgs::msg::NavSatFix, IMC::GpsFix> gpsfix_server(imc_handle, "gps_fix");
+    // ros_to_imc::BridgeServer<geometry_msgs::msg::Pose, IMC::Goto> goto_server_dummy(imc_handle, "goto_input");
+    // ros_to_imc::BridgeServer<sensor_msgs::msg::NavSatFix, IMC::GpsNavData> gpsnavdata_server(imc_handle, "gps_nav_data");
+    
+    // ros_to_imc::BridgeServer<imc_ros_bridge::msg::EstimatedState, IMC::EstimatedState> estimatedstate_server(imc_handle, "estimated_state");
+    // ros_to_imc::BridgeServer<imc_ros_bridge::msg::PlanControlState, IMC::PlanControlState> plancontrolstate_server(imc_handle, "plan_control_state");
+    // ros_to_imc::BridgeServer<imc_ros_bridge::msg::VehicleState, IMC::VehicleState> vehiclestate_server(imc_handle, "vehicle_state");
+    // ros_to_imc::BridgeServer<imc_ros_bridge::msg::RemoteState, IMC::RemoteState> RemoteState_server(imc_handle, "remote_state");
+    // ros_to_imc::BridgeServer<imc_ros_bridge::msg::SonarData, IMC::SonarData> sonar_server(imc_handle, "sonar_data");
+    // ros_to_imc::BridgeServer<imc_ros_bridge::msg::DesiredHeading, IMC::DesiredHeading> DesiredHeading_server(imc_handle, "desired_heading");
+    // ros_to_imc::BridgeServer<imc_ros_bridge::msg::DesiredHeadingRate, IMC::DesiredHeadingRate> DesiredHeadingRate_server(imc_handle, "desired_heading_rate");
+    // ros_to_imc::BridgeServer<imc_ros_bridge::msg::DesiredPitch, IMC::DesiredPitch> DesiredPitch_server(imc_handle, "desired_pitch");
+    // ros_to_imc::BridgeServer<imc_ros_bridge::msg::DesiredRoll, IMC::DesiredRoll> DesiredRoll_server(imc_handle, "desired_roll");
+    // ros_to_imc::BridgeServer<imc_ros_bridge::msg::DesiredSpeed, IMC::DesiredSpeed> DesiredSpeed_server(imc_handle, "desired_speed");
+    // ros_to_imc::BridgeServer<imc_ros_bridge::msg::DesiredZ, IMC::DesiredZ> DesiredZ_server(imc_handle, "desired_z");
+    // ros_to_imc::BridgeServer<imc_ros_bridge::msg::PlanDB, IMC::PlanDB> to_imc_plandb_server(imc_handle, "plan_db");
   
     // 450
-    imc_to_ros::BridgeServer<IMC::Goto, geometry_msgs::msg::Pose> goto_server(imc_handle, ros_node, "goto_waypoint");
-    // 150
-    imc_to_ros::BridgeServer<IMC::Heartbeat, std_msgs::msg::Empty> imc_heartbeat_server(imc_handle, ros_node, "imc_heartbeat");
-    // 550
-    imc_to_ros::BridgeServer<IMC::Abort, std_msgs::msg::Empty> abort_server(imc_handle, ros_node, "abort");
-    // 556
-    imc_to_ros::BridgeServer<IMC::PlanDB, imc_ros_bridge::PlanDB> plandb_server(imc_handle, ros_node, "plan_db");
-    imc_to_ros::BridgeServer<IMC::PlanControl, imc_ros_bridge::PlanControl> plancontrol_server(imc_handle, ros_node, "plan_control");
+    // imc_to_ros::BridgeServer<IMC::Goto, geometry_msgs::msg::Pose> goto_server(imc_handle, ros_node, "goto_waypoint");
+    // // 150
+    // imc_to_ros::BridgeServer<IMC::Heartbeat, std_msgs::msg::Empty> imc_heartbeat_server(imc_handle, ros_node, "imc_heartbeat");
+    // // 550
+    // imc_to_ros::BridgeServer<IMC::Abort, std_msgs::msg::Empty> abort_server(imc_handle, ros_node, "abort");
+    // // 556
+    // imc_to_ros::BridgeServer<IMC::PlanDB, imc_ros_bridge::msg::PlanDB> plandb_server(imc_handle, ros_node, "plan_db");
+    // imc_to_ros::BridgeServer<IMC::PlanControl, imc_ros_bridge::msg::PlanControl> plancontrol_server(imc_handle, ros_node, "plan_control");
 
-    auto announce_callback = [&](const ros::TimerEvent&) { imc_handle.announce(); };
-    auto heartbeat_callback = [&](const ros::TimerEvent&) { imc_handle.publish_heartbeat(); };
+    // auto announce_callback = [&](const ros::TimerEvent&) { imc_handle.announce(); };
+    // in ros2
+    // auto announce_callback = [&](const rclcpp::TimerBase::SharedPtr) { imc_handle.announce(); };
+    // auto heartbeat_callback = [&](const ros::TimerEvent&) { imc_handle.publish_heartbeat(); };
+    // in ros2
+    // auto heartbeat_callback = [&](const rclcpp::TimerBase::SharedPtr) { imc_handle.publish_heartbeat(); };
 
-    ros::Timer announce_timer = ros_node.createTimer(ros::Duration(10.), announce_callback);
-    ros::Timer heartbeat_timer = ros_node.createTimer(ros::Duration(1.), heartbeat_callback);
+    // ros::Timer announce_timer = ros_node.createTimer(ros::Duration(10.), announce_callback);
+    // in ros2
+    // auto handle_announce_timer = std::make_shared<IMCHandle>();
+    // auto announce_timer = ros_node->create_wall_timer(std::chrono::seconds(10), [handle_announce_timer](){handle_announce_timer->announce();});
+    // ros::Timer heartbeat_timer = ros_node.createTimer(ros::Duration(1.), heartbeat_callback);
+    // rclcpp::TimerBase::SharedPtr heartbeat_timer = ros_node.create_wall_timer(std::chrono::seconds(1), IMCHandle::publish_heartbeat);
 
-    ros::spin();
-
+    rclcpp::shutdown();
     return 0;
 }
